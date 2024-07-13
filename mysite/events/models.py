04026317ff3a16ff85d87887825feea4cf7ae867
Ahani_Samira <Ahani29@gmail.com>
2024-07-14 00:55:47 +0330

    Create a new model in events

--- mysite/events/models.py
+++ mysite/events/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Event(models.Model):
+    name = models.CharField(max_length=150)
+    data = models.DateTimeField("Event date")
+    location = models.CharField(max_length=250)
+    
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -11,25 +11,11 @@ def index(request):
         "latest_question_list": latest_question_list,
     }
     return HttpResponse(template.render(context, request))
-#def index(request):
-#    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-#    output = ", ".join([q.question_text for q in latest_question_list])
-#    return HttpResponse(output)
-#def index(request):
-#    return HttpResponse("Hello, world. You're at the polls index.")
 
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, "polls/detail.html", {"question": question})
-#def detail(request, question_id):
-#    try:
-#        question = Question.objects.get(pk=question_id)
-#    except Question.DoesNotExist:
-#        raise Http404("Question does not exist")
-#    return render(request, "polls/detail.html", {"question": question})
-#def detail(request, question_id):
-#    return HttpResponse("You're looking at question %s." % question_id)
 
 
 def results(request, question_id):
