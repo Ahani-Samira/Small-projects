eca4cc7ac00795f34a001b9518cd52e9a187c4c6
Ahani_Samira <Ahani29@gmail.com>
2024-07-18 19:33:42 +0330

    Updating the upcoming_events view

--- mysite/events/views.py
+++ mysite/events/views.py
@@ -1,3 +1,4 @@
+from datetime import timedelta
 from django.utils import timezone
 from django.shortcuts import render
 from .models import Event
@@ -5,5 +6,6 @@ from .models import Event
 
 def upcoming_events(request):
     today = timezone.now().date()
-    upcoming_list = Event.objects.filter(date__gt=today)
+    last_week = today - timedelta(days=7)
+    upcoming_list = Event.objects.filter(date__gt=last_week)
     return render(request, "events/upcoming_events.html", {"upcoming": upcoming_list})
