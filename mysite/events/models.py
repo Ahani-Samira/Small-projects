db6fde4d97fb35c576ea345f8144b00773ac7a7f
Ahani_Samira <Ahani29@gmail.com>
2024-07-18 18:19:00 +0330

    Add view

--- mysite/events/models.py
+++ mysite/events/models.py
@@ -3,6 +3,8 @@ from django.db import models
 
 class Event(models.Model):
     name = models.CharField(max_length=150)
-    data = models.DateTimeField("Event date")
+    date = models.DateTimeField("Event date")
     location = models.CharField(max_length=250)
-    
+
+    def __str__(self):
+        return self.name
--- mysite/events/views.py
+++ mysite/events/views.py
@@ -1,4 +1,9 @@
+from django.utils import timezone
 from django.shortcuts import render
+from .models import Event
 
-def  upcoming_events():
-    pass
+
+def upcoming_events(request):
+    today = timezone.now().date()
+    upcoming_list = Event.objects.filter(date__gt=today)
+    return render(request, "events/upcoming_events.html", {"upcoming": upcoming_list})
