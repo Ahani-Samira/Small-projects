0a44cd566926f2f6ae432a8cee317fbe28972c1b
Ahani_Samira <Ahani29@gmail.com>
2024-07-18 18:52:26 +0330

    Add field description to event

--- mysite/events/models.py
+++ mysite/events/models.py
@@ -5,6 +5,7 @@ class Event(models.Model):
     name = models.CharField(max_length=150)
     date = models.DateTimeField("Event date")
     location = models.CharField(max_length=250)
+    description = models.TextField(blank=True)
 
     def __str__(self):
         return self.name
