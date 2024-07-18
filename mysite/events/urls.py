5e53f04b3e6b77ab196ea794298490f72db07552
Ahani_Samira <Ahani29@gmail.com>
2024-07-19 01:19:46 +0330

    Set the URL for detail.html

--- mysite/events/urls.py
+++ mysite/events/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = "events"
 urlpatterns = [
     path("upcoming/", views.upcoming_events, name="upcoming_events"),
+    path("<int:event_id>/", views.detail, name="detail"),
     ]
