060d91ce136d3259fa8cd8c3114cc7a1a15c1da0
Ahani_Samira <Ahani29@gmail.com>
2024-07-25 12:27:14 +0330

    Created <Trip> model

--- travel_booking/bookings/models.py
+++ travel_booking/bookings/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from .validators import validate_min_one
 
-# Create your models here.
+
+class Trip(models.Model):
+    destination = models.CharField(max_length=100)
+    departure_date = models.DateField("Departure Date")
+    return_date = models.DateField("Return Date")
+    number_of_travelers = models.IntegerField(validators=[validate_min_one])
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.destination
