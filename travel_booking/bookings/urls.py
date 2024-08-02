4543e966ff2a2808faa5046d4592b8bdbe70daed
Ahani_Samira <Ahani29@gmail.com>
2024-08-02 23:07:08 +0330

    Add URL for update_trip

--- travel_booking/bookings/urls.py
+++ travel_booking/bookings/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import create_trip_view, list_trips_view
+from .views import create_trip_view, list_trips_view, update_trip_view
 
 app_name = 'bookings'
 urlpatterns = [
     path("trips/new/", create_trip_view, name="create_trip"),
     path("trips/", list_trips_view, name="list_trips"),
+    path("trips/<int:pk>/edit/", update_trip_view, name="update_trip"),
 ]
