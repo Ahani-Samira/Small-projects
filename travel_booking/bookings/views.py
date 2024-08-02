7275001a6ded2bc7d13e9c0a083da07673924de7
Ahani_Samira <Ahani29@gmail.com>
2024-08-02 19:34:04 +0330

    Add create_trip_view

--- travel_booking/bookings/views.py
+++ travel_booking/bookings/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .forms import TripForm
 
-# Create your views here.
+
+def create_trip_view(request):
+    if request.method == "POST":
+        form = TripForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = TripForm()
+    return render(request,"bookings/create_trip.html", {"form": form})
+
