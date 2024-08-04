b80e179c38c1c830794e79c73e36e0616b05ad3c
Ahani-Samira <140585948+Ahani-Samira@users.noreply.github.com>
2024-08-04 14:39:27 +0330

    Feature/date validation (#8)
    * Add date validation to validators.py
    
    * Validate the dates in the models.py

--- travel_booking/bookings/models.py
+++ travel_booking/bookings/models.py
@@ -1,10 +1,11 @@
 from django.db import models
-from .validators import validate_min_one
+from .validators import validate_min_one, validate_not_past_date, validate_time_does_not_go_back
+from django.utils import timezone
 
 
 class Trip(models.Model):
     destination = models.CharField(max_length=100)
-    departure_date = models.DateField("Departure Date")
+    departure_date = models.DateField("Departure Date", default=timezone.now(), validators=[validate_not_past_date])
     return_date = models.DateField("Return Date")
     number_of_travelers = models.IntegerField(validators=[validate_min_one])
     created_at = models.DateTimeField(auto_now_add=True)
@@ -12,3 +13,8 @@ class Trip(models.Model):
 
     def __str__(self):
         return self.destination
+
+    def clean(self):
+        super().clean()
+        if self.return_date and self.departure_date:
+            validate_time_does_not_go_back(self.departure_date, self.return_date)
